823f2c1449c88f86e7ba46a7c3827b1611a19f5c
John Dong <johndong3@hotmail.com>
2021-08-10 11:48:51 -0400

    added graphics

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from random import random
 
 from pynput.keyboard import Key, Listener
 from threading import Timer
-import time
+import pygame
+import pygame.freetype
+import constants
 
 
 class RepeatedTimer(object):
@@ -42,13 +44,48 @@ class RepeatedTimer(object):
 
 
 class Board:
+    snakeImg = pygame.image.load('snake-graphics.png')
+    parts = {
+        (1, 0, 3): (192, 0),
+        (0, 1, 3): (192, 64),
+        (-1, 0, 3): (256, 64),
+        (0, -1, 3): (256, 0),
+        (1, 0, 4): (192, 128),
+        (0, 1, 4): (192, 192),
+        (-1, 0, 4): (256, 192),
+        (0, -1, 4): (256, 128),
+        (1, 0, 1, 0): (128, 64),
+        (-1, 0, -1, 0): (128, 64),
+        (0, 1, 0, 1): (64, 0),
+        (0, -1, 0, -1): (64, 0),
+        (0, -1, 1, 0): (0, 0),
+        (-1, 0, 0, 1): (0, 0),
+        (0, -1, -1, 0): (0, 64),
+        (1, 0, 0, 1): (0, 64),
+        (0, 1, 1, 0): (128, 0),
+        (-1, 0, 0, -1): (128, 0),
+        (0, 1, -1, 0): (128, 128),
+        (1, 0, 0, -1): (128, 128),
+        2: (0, 192),
+        0: (64, 64)
+    }
+    colours = {
+        0: constants.WHITE,
+        1: constants.RED,
+        2: constants.BLUE,
+        3: constants.ORANGE
+    }
 
-    def __init__(self, snake, length=10, width=10, food=1):
-        self.length = length
-        self.width = width
-        self.grid = [[0]*width for i in range(length)]
-        self.food = food
-        self.score = 0
+    def __init__(self, snake):
+        self.screen = pygame.display.set_mode((constants.WIDTH + 50, constants.HEIGHT + 100))
+        pygame.display.set_caption('Snake')
+        pygame.freetype.init()
+        self.GAME_FONT = pygame.freetype.SysFont('Verdana', 50)
+
+        self.length = constants.ROWS
+        self.width = constants.COLS
+        self.grid = [[0]*self.width for i in range(self.length)]
+        self.food = constants.FOOD
         self.snake = snake
 
         square = snake.head
@@ -57,10 +94,12 @@ class Board:
             square = tuple(map(operator.add, square, i))
             self.update_square(square, 1)
         self.new_food()
-
+        self.grid[self.snake.head[0]][self.snake.head[1]] = 3
+        self.grid[self.snake.tail[0]][self.snake.tail[1]] = 4
         self.print_board()
-        self.rt = RepeatedTimer(0.5, self.update)
-        # self.update()
+        self.rt = RepeatedTimer(1/constants.SPEED, self.update)
+        # while True:
+        #     self.update()
 
     def update(self):
         if self.snake.growth == 0:
@@ -69,12 +108,17 @@ class Board:
         x, y = self.snake.head[0], self.snake.head[1]
         if self.in_range(self.snake.head) and self.grid[x][y] != 1:
             if self.grid[x][y] == 2:
-                self.snake.growth += self.grid[x][y] * self.food / 2
+                self.snake.growth += constants.FOOD
+                self.snake.score += constants.FOOD
                 self.new_food()
-            self.grid[x][y] = 1
+            self.grid[x][y] = 3
+            self.grid[self.snake.tail[0]][self.snake.tail[0]] = 4
             self.print_board()
+            self.grid[x][y] = 1
         else:
             print("You Died!")
+            self.GAME_FONT.render_to(self.screen, (300, 10), "You Died!", fgcolor=constants.YELLOW)
+            pygame.display.update()
             self.rt.stop()
         return
 
@@ -89,20 +133,62 @@ class Board:
         self.grid[tup[0]][tup[1]] = value
         return True
 
+    def add_part(self, target, coords):
+        surface = pygame.Surface((64, 64))
+        surface.blit(self.snakeImg, (0, 0), target + (64, 64))
+        row, col = coords[1], coords[0]
+        self.screen.blit(surface, (row * constants.SQUARE_SIZE + 25, col * constants.SQUARE_SIZE + 75))
+
     def print_board(self):
-        print('\n'.join([''.join(['{:4}'.format(item) for item in row])
-                         for row in self.grid]))
-        print(f"Score: {self.snake.score}")
+        self.screen.fill(constants.BLACK)
+        self.add_part(self.parts[2], self.foodc)
+
+        coords = self.snake.head
+        target = self.parts[self.snake.direction + (3,)]
+        self.add_part(target, coords)
+        for i in range(len(self.snake.body)):
+            dir = self.snake.body[i]
+            coords = tuple(map(operator.add, coords, dir))
+            if i == len(self.snake.body) - 1:
+                target = self.parts[dir + (4,)]
+            else:
+                next_dir = self.snake.body[i+1]
+                target = self.parts[dir + next_dir]
+            self.add_part(target, coords)
+
+
+        # for row in range(constants.ROWS):
+        #     for col in range(constants.COLS):
+        #         value = self.grid[col][row]
+        #         if value == 0 or value == 2:
+        #             target = self.parts[value]
+        #         elif value == 3 or value == 4:
+        #             key = self.snake.direction + (value,)
+        #             target = self.parts[key]
+        #         else:
+        #             target = self.parts[self.snake.direction + self.snake.body[0]]
+        #         surface = pygame.Surface((64, 64))
+        #         surface.blit(self.snakeImg, (0, 0), target + (64, 64))
+        #         self.screen.blit(surface, (row*constants.SQUARE_SIZE+25, col*constants.SQUARE_SIZE+75))
+
+        self.GAME_FONT.render_to(self.screen, (10, 10), f"Score: {self.snake.score}", fgcolor=constants.YELLOW)
+        pygame.display.update()
 
     def new_food(self):
-        index = math.floor(random()*self.length*self.width-self.snake.score)
-        for a in range(self.length):
-            for b in range(self.width):
-                if self.grid[a][b] == 0:
-                    index -= 1
-                    if index < 0:
-                        self.grid[a][b] = 2
-                        return
+        index = math.floor(random()*(self.length*self.width-self.snake.score))
+        print(index)
+        while True:
+            for a in range(self.length):
+                for b in range(self.width):
+                    if self.grid[a][b] == 0:
+                        index -= 1
+                        if index < 0:
+                            self.foodc = (a, b)
+                            self.grid[a][b] = 2
+                            print(self.foodc)
+                            return
+            print(f"not found: {index}")
+        return
 
 
 class Snake:
@@ -128,7 +214,6 @@ class Snake:
             new_dir = self.directions.get(key)
             if abs(new_dir[0] - self.direction[0]) == 1:
                 self.direction = new_dir
-                print(self.direction)
 
     def update(self):
         self.head = tuple(map(operator.sub, self.head, self.direction))
@@ -137,7 +222,6 @@ class Snake:
             self.tail = tuple(map(operator.sub, self.tail, self.body.pop(-1)))
         else:
             self.growth -= 1
-            self.score += 1
         return
 
 
@@ -152,6 +236,15 @@ def on_release(key):
 
 snake = Snake()
 board = Board(snake)
+run = True
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pass
+
+pygame.quit()
+
 
